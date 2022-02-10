c9c0b75533a6c94494773747e15b90af46f6f97e
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-10 21:03:11 +0700

    Belajar Variabel Di Kotlin Var dan Val

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,7 @@
 fun main() {
     println("Hello World")
     getHello()
+    message()
 }
 
 /*membuat function di kotlin yaitu dengan 'fun nama_function () { ... }'*/
@@ -9,3 +10,24 @@ fun getHello() {
     println("Hello Saya Dari Function getHello")
 }
 
+/*Belajar Variabel kotlin
+* Val => variabel yang tidak bisa diubah nilainya
+* Var => Variabel yang bisa diubah nilainya
+* */
+fun message() {
+    val message = "Hai"
+    println(message)
+
+    var name = "Tegar"
+    name = "Penemuan"
+    println(name)
+}
+
+/*Output
+* main() => Hello World
+*
+* getHello() => Hello Saya Dari Function getHello
+*
+* message() => val -> Hai
+*              var -> Penemuan
+* */

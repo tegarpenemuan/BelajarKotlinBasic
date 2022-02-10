d7948c5a4476f56715b81c489dd23d4d456a68cd
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-10 21:15:49 +0700

    Belajar Data Type Kotlin

--- src/main.kt
+++ src/main.kt
@@ -1,16 +1,17 @@
-/*fun main() adalah function yang dijalankan pertama kali atau disebut dengan entri point*/
+/**fun main() adalah function yang dijalankan pertama kali atau disebut dengan entri point*/
 fun main() {
     println("Hello World")
     getHello()
     message()
+    data()
 }
 
-/*membuat function di kotlin yaitu dengan 'fun nama_function () { ... }'*/
+/**membuat function di kotlin yaitu dengan 'fun nama_function () { ... }'*/
 fun getHello() {
     println("Hello Saya Dari Function getHello")
 }
 
-/*Belajar Variabel kotlin
+/**Belajar Variabel kotlin
 * Val => variabel yang tidak bisa diubah nilainya
 * Var => Variabel yang bisa diubah nilainya
 * */
@@ -23,11 +24,38 @@ fun message() {
     println(name)
 }
 
-/*Output
+
+/**Belajar Data Types atau Type Data
+ *
+ * Penulisan: 'var namaVariabel: TypeData = value
+ *
+ * Contoh:
+ *      String  => var name: String = "Tegar Penemuan"
+ *      Integer => var age: Int = 20
+ *
+ * Jika dipanggil:
+ * print("Nama Saya $name dan Umur Saya $umur")
+ */
+fun data() {
+    val name = "Tegar Penemuan"
+    val age = 20
+
+    println("Nama Saya $name dan Umur Saya $age Tahun")
+}
+
+
+
+
+
+
+/**Output
 * main() => Hello World
 *
 * getHello() => Hello Saya Dari Function getHello
 *
 * message() => val -> Hai
-*              var -> Penemuan
+*              var -> Penemuan*
+ *              
+ * data() => Nama Saya Tegar Penemuan dan Umur Saya 20 Tahun    
+ *          
 * */

8f09836cea428bccc8753afb67effc24437f0b57
Tegar Penemuan <tegarpenemuan@gmail.com>
2022-02-10 20:59:44 +0700

    Belajar Function Di Kotlin

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,11 @@
-//fun main() adalah function yang dijalankan pertama kali atau disebut dengan entri point
+/*fun main() adalah function yang dijalankan pertama kali atau disebut dengan entri point*/
 fun main() {
     println("Hello World")
+    getHello()
 }
+
+/*membuat function di kotlin yaitu dengan 'fun nama_function () { ... }'*/
+fun getHello() {
+    println("Hello Saya Dari Function getHello")
+}
+
